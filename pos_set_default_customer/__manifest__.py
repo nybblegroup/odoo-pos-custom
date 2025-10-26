3f7037f8efb0acd21490a1b7349639ff3b1a0851
Cristiam Carreno <cristiamcarreno@gmail.com>
2025-10-25 22:20:31 -0500

    [NORMIG-389][UPD] l10n_ar_pos_einvoice, l10n_ar_pos_fields_partner - Improve POS AR einvoice and partner fields, add validations and UI enhancements

--- pos_set_default_customer/__manifest__.py
+++ pos_set_default_customer/__manifest__.py
@@ -1,26 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'POS Default Customer',
-    'summary': "Set Default Customer in POS",
-    'description': 'Set Default Customer in POS',
-
-    'author': 'iPredict IT Solutions Pvt. Ltd.',
-    'website': 'http://ipredictitsolutions.com',
+    "name": "POS Default Customer",
+    "summary": "Set Default Customer in POS",
+    "description": "Set Default Customer in POS",
+    "author": "iPredict IT Solutions Pvt. Ltd.",
+    "website": "http://ipredictitsolutions.com",
     "support": "ipredictitsolutions@gmail.com",
-
-    'category': 'Point of Sale',
-    'version': '17.0.0.1.0',
-    'depends': ['point_of_sale'],
-
-    'data': [
-        'views/assets.xml',
-        'views/pos_config_view.xml',
+    "category": "Point of Sale",
+    "version": "18.0.0.1.0",
+    "depends": ["point_of_sale"],
+    "data": [
+        "views/assets.xml",
+        "views/pos_config_view.xml",
     ],
-
-    'license': "OPL-1",
-
-    'installable': False,
-    'application': True,
-
-    'images': ['static/description/banner.png'],
+    "license": "OPL-1",
+    "installable": False,
+    "application": True,
+    "images": ["static/description/banner.png"],
 }
